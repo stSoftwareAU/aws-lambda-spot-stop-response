a3e55d89ad24674a4a9952fda5809d0138b104d2
Nigel Leck <nigel@stsoftware.com.au>
2019-05-04 17:54:41 +1000

    first start

---
+++ run.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+ID=`curl http://169.254.169.254/latest/meta-data/instance-id`
+
+instanceJSON=`aws ec2 describe-instances --instance-ids $ID`
+
+asName=$( jq -r '.Reservations[0].Instances[0].Tags[]| select(.Key == "aws:autoscaling:groupName") .Value'<<<${instanceJSON} )
+
+if [[ -z $asName ]]; then
+   echo "no autoscale groupd for: $ID"
+   exit 1;
+fi
+
+while true
+do
+  res=$(curl -s -o /var/log/ec2-user/spot-termination.json -w '%{http_code}\n' http://169.254.169.254/latest/meta-data/spot/instance-action)
+
+  if ((res == 200)); then
+    set -x			# activate debugging from here
+
+    # OK We have << 2 minutes to complete all the work. 
+    # Start coping the logs in the background.
+    echo "shutting down" |tee /var/log/ec2-user/shut_down.txt
+
+    asJSON=`aws autoscaling describe-auto-scaling-groups --auto-scaling-group-name $asName`
+
+    launchConfigurationName=$( jq -r '.AutoScalingGroups[0].LaunchConfigurationName'<<<${asJSON} )
+    costlyConfigurationName="${launchConfigurationName/\#spot/#costly}";
+    if [[ "$launchConfigurationName" != "$costlyConfigurationName" ]]; then
+      aws autoscaling update-auto-scaling-group --auto-scaling-group-name $asName \
+          --launch-configuration-name $costlyConfigurationName 
+
+      if [[ $asName =~ .*-web ]]; then
+        minSize=$( jq -r '.AutoScalingGroups[0].MinSize'<<<${asJSON} );
+        if [[ $minSize < 2 ]]; then
+          desiredCapacity=$( jq -r '.AutoScalingGroups[0].DesiredCapacity'<<<${asJSON} );
+
+          maxSize=$( jq -r '.AutoScalingGroups[0].MaxSize'<<<${asJSON} );
+          increaseCapacity=$(($desiredCapacity + 1))
+          if [[ $increaseCapacity > $maxSize ]]; then
+            increaseCapacity=$maxSize;
+          fi
+
+          if [[ $increaseCapacity < $minSize ]]; then
+            increaseCapacity=$minSize;
+          fi
+          aws autoscaling update-auto-scaling-group --auto-scaling-group-name $asName \
+              --desired-capacity $increaseCapacity \
+              --min-size $increaseCapacity
+        fi
+      fi
+    fi
+
+    # Stop monitoring once notified of termination.
+    bash -x ../drainInstance.sh $ID
+  
+    # if we have removed the instance from the load balancer then we need to make sure this this instance actually shuts down.
+    echo "The instance should be terminated in less than 2 minutes but just shutdown in five if this is not the case for some reason"
+    sudo shutdown +5
+    
+    echo "$launchConfigurationName -> $costlyConfigurationName" |mailx \
+	-a /var/log/ec2-user/termination_notice.log \
+	-a /var/log/ec2-user/spot-termination.json \
+	-s "Spot $ID terminated for $asName" support@stsoftware.com.au	
+
+    exit
+  else
+    sleep 5
+  fi
+
+done
