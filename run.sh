b1b782bf693d4d53fb992a61e1fe9392a67a6559
Nigel Leck <monster.flying.s@gmail.com>
2019-05-04 21:00:34 +1000

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,77 +1,87 @@
 #!/bin/bash
 set -e
+init() {
+	identityJSON=`curl -s http://169.254.169.254/latest/dynamic/instance-identity/document`
 
-identityJSON=`curl -s http://169.254.169.254/latest/dynamic/instance-identity/document`
+	instanceId=$( jq -r '.instanceId'<<<${identityJSON} )
+	region=$( jq -r '.region'<<<${identityJSON} )
 
-instanceId=$( jq -r '.instanceId'<<<${identityJSON} )
-region=$( jq -r '.region'<<<${identityJSON} )
+	instanceJSON=`aws ec2 describe-instances --instance-ids ${instanceId} --region ${region}`
 
-instanceJSON=`aws ec2 describe-instances --instance-ids ${instanceId} --region ${region}`
+	asName=$( jq -r '.Reservations[0].Instances[0].Tags[]| select(.Key == "aws:autoscaling:groupName") .Value'<<<${instanceJSON} )
 
-asName=$( jq -r '.Reservations[0].Instances[0].Tags[]| select(.Key == "aws:autoscaling:groupName") .Value'<<<${instanceJSON} )
+	if [[ -z $asName ]]; then
+	   echo "no autoscale groupd for: $ID"
+	   exit 1;
+	fi
+}
 
-if [[ -z $asName ]]; then
-   echo "no autoscale groupd for: $ID"
-   exit 1;
-fi
+notified() {
+	set -x			# activate debugging from here
 
-#Don't stop no matter what ( we will not be restarted). 
-set +e
-while true
-do
-  res=$(curl -s -o /var/log/ec2-user/spot-termination.json -w '%{http_code}\n' http://169.254.169.254/latest/meta-data/spot/instance-action)
+	# OK We have << 2 minutes to complete all the work. 
+	# Start coping the logs in the background.
+	echo "shutting down" |tee /var/log/ec2-user/shut_down.txt
 
-  if ((res == 200)); then
-    set -x			# activate debugging from here
+	asJSON=`aws autoscaling describe-auto-scaling-groups --auto-scaling-group-name $asName`
 
-    # OK We have << 2 minutes to complete all the work. 
-    # Start coping the logs in the background.
-    echo "shutting down" |tee /var/log/ec2-user/shut_down.txt
+	launchConfigurationName=$( jq -r '.AutoScalingGroups[0].LaunchConfigurationName'<<<${asJSON} )
+	costlyConfigurationName="${launchConfigurationName/\#spot/#costly}";
+	if [[ "$launchConfigurationName" != "$costlyConfigurationName" ]]; then
+		aws autoscaling update-auto-scaling-group --auto-scaling-group-name $asName \
+	--launch-configuration-name $costlyConfigurationName 
 
-    asJSON=`aws autoscaling describe-auto-scaling-groups --auto-scaling-group-name $asName`
+		if [[ $asName =~ .*-web ]]; then
+	minSize=$( jq -r '.AutoScalingGroups[0].MinSize'<<<${asJSON} );
+	if [[ $minSize < 2 ]]; then
+	desiredCapacity=$( jq -r '.AutoScalingGroups[0].DesiredCapacity'<<<${asJSON} );
 
-    launchConfigurationName=$( jq -r '.AutoScalingGroups[0].LaunchConfigurationName'<<<${asJSON} )
-    costlyConfigurationName="${launchConfigurationName/\#spot/#costly}";
-    if [[ "$launchConfigurationName" != "$costlyConfigurationName" ]]; then
-      aws autoscaling update-auto-scaling-group --auto-scaling-group-name $asName \
-          --launch-configuration-name $costlyConfigurationName 
+	maxSize=$( jq -r '.AutoScalingGroups[0].MaxSize'<<<${asJSON} );
+	increaseCapacity=$(($desiredCapacity + 1))
+	if [[ $increaseCapacity > $maxSize ]]; then
+		increaseCapacity=$maxSize;
+	fi
 
-      if [[ $asName =~ .*-web ]]; then
-        minSize=$( jq -r '.AutoScalingGroups[0].MinSize'<<<${asJSON} );
-        if [[ $minSize < 2 ]]; then
-          desiredCapacity=$( jq -r '.AutoScalingGroups[0].DesiredCapacity'<<<${asJSON} );
+	if [[ $increaseCapacity < $minSize ]]; then
+		increaseCapacity=$minSize;
+	fi
+	aws autoscaling update-auto-scaling-group --auto-scaling-group-name $asName \
+			--desired-capacity $increaseCapacity \
+			--min-size $increaseCapacity
+	fi
+		fi
+	fi
 
-          maxSize=$( jq -r '.AutoScalingGroups[0].MaxSize'<<<${asJSON} );
-          increaseCapacity=$(($desiredCapacity + 1))
-          if [[ $increaseCapacity > $maxSize ]]; then
-            increaseCapacity=$maxSize;
-          fi
+	# Stop monitoring once notified of termination.
+	bash -x ../drainInstance.sh $ID
 
-          if [[ $increaseCapacity < $minSize ]]; then
-            increaseCapacity=$minSize;
-          fi
-          aws autoscaling update-auto-scaling-group --auto-scaling-group-name $asName \
-              --desired-capacity $increaseCapacity \
-              --min-size $increaseCapacity
-        fi
-      fi
-    fi
+	# if we have removed the instance from the load balancer then we need to make sure this this instance actually shuts down.
+	echo "The instance should be terminated in less than 2 minutes but just shutdown in five if this is not the case for some reason"
+	sudo shutdown +5
 
-    # Stop monitoring once notified of termination.
-    bash -x ../drainInstance.sh $ID
-  
-    # if we have removed the instance from the load balancer then we need to make sure this this instance actually shuts down.
-    echo "The instance should be terminated in less than 2 minutes but just shutdown in five if this is not the case for some reason"
-    sudo shutdown +5
-    
-    echo "$launchConfigurationName -> $costlyConfigurationName" |mailx \
-	-a /var/log/ec2-user/termination_notice.log \
-	-a /var/log/ec2-user/spot-termination.json \
-	-s "Spot $ID terminated for $asName" support@stsoftware.com.au	
+	echo "$launchConfigurationName -> $costlyConfigurationName" |mailx \
+		-a /var/log/ec2-user/termination_notice.log \
+		-a /var/log/ec2-user/spot-termination.json \
+		-s "Spot $ID terminated for $asName" support@stsoftware.com.au	
 
-    exit
-  else
-    sleep 5
-  fi
+	exit
+}
 
-done
+monitor() {
+
+	#Don't stop no matter what ( we will not be restarted). 
+	set +e
+	while true
+	do
+	  res=$(curl -s -o /var/log/ec2-user/spot-termination.json -w '%{http_code}\n' http://169.254.169.254/latest/meta-data/spot/instance-action)
+
+	  if ((res == 200)); then
+      notified
+	  else
+	    sleep 5
+	  fi
+	done
+}
+
+init
+main
