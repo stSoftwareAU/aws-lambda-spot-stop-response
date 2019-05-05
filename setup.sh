67513b639498635728ed9bca796bd6880b497263
Nigel Leck <nigel@stsoftware.com.au>
2019-05-05 22:04:47 +1000

    No need for sudo

--- setup.sh
+++ setup.sh
@@ -1,30 +1,25 @@
 #!/bin/bash
 set -ex
+
 #Defaults
 monitorUser="spot-monitor"
 
 # Install script dependencies jq
-sudo yum install -y jq git
+yum install -y jq git
 
 #fetch and install script into a low permissioned user.
-sudo useradd ${monitorUser}
-sudo chmod ugo+xr /home/${monitorUser}
+useradd ${monitorUser}
+chmod ugo+xr /home/${monitorUser}
 cd /home/${monitorUser}
 
-#Set up logs
-logFile="/var/log/${monitorUser}.log"
-touch ${logFile}
-
 #Set up ssh access to github
-sudo mkdir -p .ssh
-tmpfile=$(mktemp /tmp/known_hosts.XXXXXX)
-sudo ssh-keyscan github.com > ${tmpfile}
-sudo mv ${tmpfile} .ssh/known_hosts
-sudo chown --recursive ${monitorUser}:${monitorUser} .ssh
-sudo chmod go-rxw,u-x .ssh/*
+mkdir -p .ssh
+ssh-keyscan github.com > .ssh/known_hosts
+chown --recursive ${monitorUser}:${monitorUser} .ssh
+chmod go-rxw,u-x .ssh/*
 
 #Clone the monitor
 sudo -u ${monitorUser} git clone --depth 1 https://github.com/stSoftwareAU/aws-spot-termination-monitor.git monitor
 
 # Run the monitor in background
-sudo -u ${monitorUser} monitor/run.sh $@ > ${logFile} &
+sudo -u ${monitorUser} monitor/run.sh $@ > /var/log/${monitorUser}.log &
