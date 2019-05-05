854446b199f4a6a7452eb72557498581e1542d22
Nigel Leck <nigel@stsoftware.com.au>
2019-05-05 22:01:47 +1000

    removed argument check

--- setup.sh
+++ setup.sh
@@ -1,33 +1,7 @@
 #!/bin/bash
 set -ex
-
+#Defaults
 monitorUser="spot-monitor"
-
-# Optional: Which topic should we send the notification of the instance is about to be terminated.
-topicARN=""
-resetMinSize=""
-resetOnDemandBaseCapacity=""
-
-while [[ $# -gt 0 ]]; do
-  case "$1" in
-    --topic-arn )
-      topicARN=$2
-      shift;;
-    --reset-min-size)
-      resetMinSize=$2
-      shift
-      ;;
-    --reset-on-demand-base-capacity)
-      resetOnDemandBaseCapacity=$2
-      shift
-    * )
-      echo "Unknown parameter passed: $1"
-      exit 1 ;;
-  esac
-  if [[ $# -gt 0 ]]; then
-    shift
-  fi
-done
 
 # Install script dependencies jq
 sudo yum install -y jq git
@@ -53,17 +27,4 @@ sudo chmod go-rxw,u-x .ssh/*
 sudo -u ${monitorUser} git clone --depth 1 https://github.com/stSoftwareAU/aws-spot-termination-monitor.git monitor
 
 # Run the monitor in background
-cmd="monitor/run.sh"
-if [ ! -z "${resetOnDemandBaseCapacity}" ]; then
-  cmd="$cmd --reset-on-demand-base-capacity ${resetOnDemandBaseCapacity}"
-fi
-
-if [ ! -z "${resetMinSize}" ]; then
-  cmd="$cmd --reset-min-size ${resetMinSize}"
-fi
-
-if [ ! -z "${topicARN}" ]; then
-  cmd="$cmd --topic-arn ${topicARN}"
-fi
-
-sudo -u ${monitorUser} ${cmd} > ${logFile} &
+sudo -u ${monitorUser} monitor/run.sh $@ > ${logFile} &
